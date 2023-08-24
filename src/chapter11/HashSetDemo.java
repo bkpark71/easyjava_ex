f2842462e81c885b13a18e7ea20ac66b61685ee7
bkpark <bokyoungp@naver.com>
2023-08-24 15:31:09 +0900

    11일차 V3.1

--- src/chapter11/HashSetDemo.java
+++ src/chapter11/HashSetDemo.java
@@ -21,30 +21,26 @@ public class HashSetDemo {
     boolean e = hashSet.add("E");
     if(!e) System.out.println("추가하지 못했습니다.");
 
-
-    boolean isExist = false;
-    for (String ele : hashSet){
-      if(ele.hashCode() == "F".hashCode()){
-        isExist = true;
-        break;
-      }
-    }
-    if(!isExist) hashSet.add("F");
+    if(!isExist(hashSet, "F")) hashSet.add("F");
     else
       System.out.println("F가 이미 존재하여 추가할 수 없습니다.");
 
     System.out.println("E의 hashCode() = " + "E".hashCode());
     System.out.println("F의 hashCode() = " + "F".hashCode());
 
-    isExist = false;
-    for (String ele : hashSet){
-      if(ele.hashCode() == "G".hashCode()){
+    if(!isExist(hashSet, "G")) hashSet.add("G");
+    else
+      System.out.println("G가 이미 존재하여 추가할 수 없습니다.");
+  }
+
+  static <T> boolean isExist(Set<T> set, T obj){
+    boolean isExist = false;
+    for (T ele : set){
+      if(ele.hashCode() == obj.hashCode()){
         isExist = true;
         break;
       }
     }
-    if(!isExist) hashSet.add("G");
-    else
-      System.out.println("G가 이미 존재하여 추가할 수 없습니다.");
+    return isExist;
   }
 }
