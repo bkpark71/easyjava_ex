c25a66338e6b88d1ef8f6e3690b9ad2817564585
bkpark <bokyoungp@naver.com>
2023-08-28 12:37:59 +0900

    13일차 V2.1

---
+++ src/chapter12/Optional1Demo.java
@@ -0,0 +1,33 @@
+package chapter12;
+
+import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.OptionalInt;
+
+public class Optional1Demo {
+  public static OptionalDouble divide(double x , double y){
+    return y == 0.0 ?  OptionalDouble.empty() : OptionalDouble.of(x/y);
+  }
+
+  public static OptionalInt divide(int x , int y){
+    return y == 0 ?  OptionalInt.empty() : OptionalInt.of(x/y);
+  }
+
+  public static void main(String[] args) {
+    OptionalInt divideInt = divide(10, 2);
+    System.out.println(divideInt);
+    System.out.println(divideInt.getAsInt());
+
+    OptionalDouble divide = divide(10.0, 2.0);
+    System.out.println(divide);
+    System.out.println(divide.getAsDouble());
+    System.out.println(10/0.0);
+
+    OptionalDouble divideZero = divide(10.0, 0.0);
+    System.out.println(divideZero);
+
+    Optional<String> apple = Optional.of("apple");
+    System.out.println(apple);
+    System.out.println(apple.get());
+  }
+}
